ef898ceadd4b288102fa04de8fb1ac9887176de3
alex <alex@gmail.com>
2023-12-01 20:30:38 +0300

    task_third

--- main.py
+++ main.py
@@ -2,17 +2,48 @@ import sys
 from random import randrange
 
 from PyQt5.QtCore import QRect
+from PyQt5 import QtWidgets
 from PyQt5.QtGui import QPainter, QColor
-from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5 import uic
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5 import QtCore, QtGui
 
 
-class MyWidget(QMainWindow):
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(554, 379)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(221, 0, 111, 28))
+        self.pushButton.setCheckable(True)
+        self.pushButton.setObjectName("pushButton")
+        font = QtGui.QFont()
+        font.setPointSize(24)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 554, 28))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "Нажми меня"))
+
+
+class MyWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('Ui.ui', self)
-        self.do_paint = False
+        self.setupUi(self)
         self.pushButton.clicked.connect(self.run)
+        self.do_paint = False
 
     def run(self):
         self.do_paint = True
@@ -26,8 +57,8 @@ class MyWidget(QMainWindow):
             qp.end()
 
     def run2(self, qp):
-        radius = randrange(1, 500)
-        qp.setPen(QColor(255, 255, 0))
+        radius = randrange(1, 250)
+        qp.setPen(QColor(randrange(255), randrange(255), randrange(255)))
         qp.drawEllipse(QRect(0, 50, radius, radius))
         self.do_paint = False
 
@@ -37,3 +68,4 @@ if __name__ == '__main__':
     ex = MyWidget()
     ex.show()
     sys.exit(app.exec_())
+
